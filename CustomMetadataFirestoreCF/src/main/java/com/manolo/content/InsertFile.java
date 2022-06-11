6597e8f15c7c0b2dd713af787e3899d4b4fa25b7
Manuel Hurtado <manuel.hurtado@gmail.com>
2022-06-11 14:43:59 +0200

    updateTime 2

--- CustomMetadataFirestoreCF/src/main/java/com/manolo/content/InsertFile.java
+++ CustomMetadataFirestoreCF/src/main/java/com/manolo/content/InsertFile.java
@@ -21,13 +21,17 @@ import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.FirestoreOptions;
 import com.google.cloud.firestore.WriteResult;
 import com.google.cloud.firestore.spi.v1.FirestoreRpc;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.logging.Logger;
 
 public class InsertFile implements BackgroundFunction<GcsEvent> {
@@ -36,6 +40,7 @@ public class InsertFile implements BackgroundFunction<GcsEvent> {
   public static final String SEPARATOR = "::";
   public static final String GCS_SEPARATOR = "/";
   public static final String CONTENT_RUNTIME_VAR = "COLLECTION";
+  public static final String LAST_UPDATED_ATTRIBUTE = "_updated";
 
   public static final String  EVENT_FINALIZE = "google.storage.object.finalize";
   public static final String  EVENT_DELETE = "google.storage.object.delete";
@@ -49,6 +54,8 @@ public class InsertFile implements BackgroundFunction<GcsEvent> {
     .newBuilder().build()
     .getService()
     .get(event.getBucket(), event.getName(), Storage.BlobGetOption.fields(Storage.BlobField.values()));
+
+    logEvent(event, context, blob) ;
   
     Firestore db = FirestoreOptions.getDefaultInstance().getService();
 
@@ -61,17 +68,51 @@ public class InsertFile implements BackgroundFunction<GcsEvent> {
       case EVENT_ARCHIVE:
         doDelete(event, blob, db);
         break;
-
       default:
         logger.info("no matching event: " + context.eventType());
     }
 
   }
 
+  private void logEvent(GcsEvent event, Context context, Blob blob){
+    StringBuffer sbEvent = new StringBuffer().
+      append("bucket: " + event.getBucket()).
+      append("; name: " + event.getName()).
+      append("; metageneration: " + event.getMetageneration()).
+      append("; timeCreated: " + event.getTimeCreated().toString()).
+      append("; updated: " + event.getUpdated().toString());
+    logger.info("GcsEvent: " + sbEvent.toString());
+    
+    StringBuffer sbContext = new StringBuffer().
+      append("eventId: " + context.eventId()).
+      append("; eventType: " + context.eventType()).
+      append("; resource: " + context.resource()).
+      append("; timestamp: " + context.timestamp());
+    Map<String,String> ctxAttrs = context.attributes();
+    for(String keyCtxAttr : ctxAttrs.keySet())
+      sbContext.append("Context Attribute " + keyCtxAttr + ": " + ctxAttrs.get(keyCtxAttr));
+    logger.info("Context: " + sbContext.toString());
+
+    if(blob != null){
+      StringBuffer sbBlob = new StringBuffer().
+      append("Id: " + blob.getBlobId()).
+      append("; Generation: " + blob.getGeneration()).
+      append("; Metageneration: " + blob.getMetageneration()).
+      append("; TimeCreated: " + new Date(blob.getCreateTime())).
+      append("; Last Metadata Update: " + new Date(blob.getUpdateTime()));  
+      logger.info("Blob: " + sbBlob.toString());
+    }
+
+  }
+
   private void doFinalize(GcsEvent event, Blob blob, Firestore db) {
+    String docId = event.getBucket() + SEPARATOR + event.getName().replace(GCS_SEPARATOR, SEPARATOR); 
+
     try (FirestoreRpc rpc = (FirestoreRpc) FirestoreOptions.getDefaultInstance().getRpc();){
-      String docId = event.getBucket() + SEPARATOR + event.getName().replace(GCS_SEPARATOR, SEPARATOR);
+      if(isOldEvent(docId, event.getUpdated().getTime(), db))
+        return;
       Map<String, Object> data = new HashMap<>();
+      data.put(LAST_UPDATED_ATTRIBUTE, event.getUpdated().getTime());
       if(blob.getMetadata() != null){
         for (Map.Entry<String, String> userMetadata : blob.getMetadata().entrySet()) {
           data.put(userMetadata.getKey(), userMetadata.getValue());
@@ -84,7 +125,7 @@ public class InsertFile implements BackgroundFunction<GcsEvent> {
     }    
   }
 
-  private void doDelete(GcsEvent event, Blob blob,Firestore db) {
+  private void doDelete(GcsEvent event, Blob blob, Firestore db) {
     try (FirestoreRpc rpc = (FirestoreRpc) FirestoreOptions.getDefaultInstance().getRpc();){
       String docId = event.getBucket() + SEPARATOR + event.getName().replace(GCS_SEPARATOR, SEPARATOR);
       // Check file deleted (blob == null) or updated (blob != null). If updated do nothing
@@ -95,6 +136,23 @@ public class InsertFile implements BackgroundFunction<GcsEvent> {
     } catch (Exception e) {
       e.printStackTrace();
     }      
+  }
+
+  private boolean isOldEvent(String docId, long eventTime, Firestore db) throws InterruptedException, ExecutionException{
+    DocumentReference docRef = db.collection(System.getenv(CONTENT_RUNTIME_VAR)).document(docId);
+    ApiFuture<DocumentSnapshot> future = docRef.get();
+
+    DocumentSnapshot document = future.get();
+    if (!document.exists()) 
+      return false;
+
+    Map<String, Object> docMap = document.getData();
+    Long lastEventProcessed = (Long) docMap.get(LAST_UPDATED_ATTRIBUTE);
+   
+    if(lastEventProcessed == null || lastEventProcessed <= eventTime)
+      return false;
+    else
+      return true;
   }
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,92 @@
-# StorageCustomMetadataFirestore
+# Storage Custom Metadata Firestore sync
+
+Use Google Cloud Storage custom metadata and keep it synchronized with a Firestore database 
+
+!["Solution architecture"](img/arch_gcs_firestore.jpg?raw=true)
+
+## Installation
+
+Deployment from CloudShell console.
+Setup environment variables. Use your own:
+
+```bash
+REGION=europe-west3
+GOOGLE_CLOUD_PROJECT=`gcloud config list --format 'value(core.project)'`
+BUCKET_NAME=$GOOGLE_CLOUD_PROJECT
+COLLECTION=content
+```
+
+Create a Firestore database in Native mode. Create a Cloud Storage bucket in the same region:
+
+```bash
+gcloud app create --region=$REGION
+gcloud firestore databases create --region=$REGION
+gsutil mb -l $REGION gs://$BUCKET_NAME 
+```
+
+Create and build Cloud Functions from this repo:
+
+```bash
+git clone https://github.com/mahurtado/StorageCustomMetadataFirestore
+cd StorageCustomMetadataFirestore/CustomMetadataFirestoreCF
+mvn compile
+mvn package
+```
+
+Deploy the Cloud Functions:
+
+```bash
+gcloud services enable cloudbuild.googleapis.com
+ 
+gcloud functions deploy content-gcs-insert \
+    --set-env-vars COLLECTION=$COLLECTION \
+    --region $REGION \
+    --entry-point com.manolo.content.InsertFile \
+    --runtime java11 \
+    --memory 512MB \
+    --trigger-resource $GOOGLE_CLOUD_PROJECT \
+    --trigger-event google.storage.object.finalize \
+    --source=target/deployment
+ 
+gcloud functions deploy content-gcs-delete \
+    --set-env-vars COLLECTION=$COLLECTION \
+    --region $REGION \
+    --entry-point com.manolo.content.InsertFile \
+    --runtime java11 \
+    --memory 512MB \
+    --trigger-resource $GOOGLE_CLOUD_PROJECT \
+    --trigger-event google.storage.object.delete \
+    --source=target/deployment
+ 
+gcloud functions deploy content-gcs-metadata-update \
+    --set-env-vars COLLECTION=$COLLECTION \
+    --region $REGION \
+    --entry-point com.manolo.content.InsertFile \
+    --runtime java11 \
+    --memory 512MB \
+    --trigger-resource $GOOGLE_CLOUD_PROJECT \
+    --trigger-event google.storage.object.metadataUpdate \
+    --source=target/deployment
+ 
+gcloud functions deploy content-gcs-metadata-archive \
+    --set-env-vars COLLECTION=$COLLECTION \
+    --region $REGION \
+    --entry-point com.manolo.content.InsertFile \
+    --runtime java11 \
+    --memory 512MB \
+    --trigger-resource $GOOGLE_CLOUD_PROJECT \
+    --trigger-event google.storage.object.archive \
+    --source=target/deployment
+```
+
+## Usage
+
+For testing, just uploada filess to the configured bucket, include new metadata, modify, etc.
+Then check Firestore console and see the changes. Example for uploading a local file with custom metadata:
+
+```bash
+gsutil -h "x-goog-meta-key1:value1" cp [path_to_your_file] gs://$GOOGLE_CLOUD_PROJECT
+```
+
+## Contributing
+Pull requests are welcome. 
